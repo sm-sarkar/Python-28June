63947e4f9348ed077aecb4ced84bf23aed232118
Shivam Sarkar <64729297+sm-sarkar@users.noreply.github.com>
2021-06-29 23:06:19 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,73 @@
+'''TYPE CONVERSION'''
+
+#INTEGER TO FLOAT
+a = 10
+print(a,type(a))
+b=float(a)
+print(b,type(b))
+
+#INTEGER TO STRING
+c = 10
+print(c,type(c))
+d=str(c)
+print(d,type(d))
+
+#INTEGER TO BOOLEAN
+e = 10
+print(e,type(e))
+f=bool(e)
+print(f,type(f))
+
+#STRING TO INT
+g = "10"
+print(g,type(g))
+h=int(g)
+print(h,type(h))
+
+#STRING TO FLOAT
+i = "10"
+print(i,type(i))
+j=float(i)
+print(j,type(j))
+
+#STRING TO BOOLEAN
+k = "10"
+print(k,type(k))
+l=bool(k)
+print(l,type(l))
+
+#FLOAT TO INTEGER
+m = 10.0
+print(m,type(m))
+n=int(m)
+print(n,type(n))
+
+#FLOAT TO STRING
+o = 10.0
+print(o,type(o))
+p=str(o)
+print(p,type(p))
+
+#FLOAT TO BOOLEAN
+q = 10.0
+print(q,type(q))
+r=bool(q)
+print(r,type(r))
+
+#BOOLEAN TO INTEGER
+s = True
+print(s,type(s))
+t = int(s)
+print(t,type(t))
+
+#BOOLEAN TO STRING
+u = True
+print(u,type(u))
+v = str(u)
+print(v,type(v))
+
+#BOOLEAN TO FLOAT
+w = True
+print(w,type(w))
+x = float(w)
+print(x,type(x))
